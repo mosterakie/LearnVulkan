3ddbb95269af9885d114bd0f9ca440b4f844eb72
Akie <x178111@163.com>
2024-07-30 17:34:04 +0800

    添加项目文件。

---
+++ LearnVulkan/main.cpp
@@ -0,0 +1,46 @@
+﻿// LearnVulkan.cpp: 定义应用程序的入口点。
+//
+
+#define Log(x) std::cout<<x<<std::endl;
+
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan() {
+        Log("Init Vulkan")
+    }
+
+    void mainLoop() {
+
+    }
+
+    void cleanup() {
+
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
